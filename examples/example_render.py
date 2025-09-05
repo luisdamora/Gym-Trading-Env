aeed8b8450cab831d5fe0b4e748285382f2775b0
luis david <11556208+luisdamora@users.noreply.github.com>
2025-09-04 21:39:04 -0500

    feat: Actualizar dependencias y configuración
    - Se añadió Ruff para formateo y linting de código en pyproject.toml.
    - Se actualizaron las importaciones en varios módulos para usar `gymnasium` en lugar de `gym`.
    - Se realizaron ajustes menores en el manejo de rutas y nombres de archivo.

--- examples/example_render.py
+++ examples/example_render.py
@@ -1,15 +1,18 @@
-import sys  
+import sys
+
 sys.path.append("./src")
 
-import pandas as pd
 from gym_trading_env.renderer import Renderer
-
 
 renderer = Renderer(render_logs_dir="render_logs")
 
 # # Add Custom Lines (Simple Moving Average)
-# renderer.add_line( name= "sma10", function= lambda df : df["close"].rolling(10).mean(), line_options ={"width" : 1, "color": "purple"})
-# renderer.add_line( name= "sma20", function= lambda df : df["close"].rolling(20).mean(), line_options ={"width" : 1, "color": "blue"})
+# renderer.add_line(
+#     name= "sma10", function= lambda df : df["close"].rolling(10).mean(), line_options ={"width" : 1, "color": "purple"}
+# )
+# renderer.add_line(
+#     name= "sma20", function= lambda df : df["close"].rolling(20).mean(), line_options ={"width" : 1, "color": "blue"}
+# )
 
 # # Add Custom Metrics (Annualized metrics)
 # renderer.add_metric(
--- examples/example_vectorized_multi_environment.py
+++ examples/example_vectorized_multi_environment.py
@@ -1,30 +1,31 @@
-import sys  
+import sys
+
 sys.path.append("./src")
 
-import pandas as pd
-import numpy as np
+
 import gymnasium as gym
-import datetime
-
-import gym_trading_env
-from gym_trading_env.downloader import download
+import numpy as np
+import pandas as pd
 
 
-def add_features(df : pd.DataFrame):
+def add_features(df: pd.DataFrame):
     df["feature_close"] = df["close"].pct_change()
-    df["feature_open"] = df["open"]/df["close"]
-    df["feature_high"] = df["high"]/df["close"]
-    df["feature_low"] = df["low"]/df["close"]
-    df["feature_volume"] = df["volume"] / df["volume"].rolling(7*24).max()
-    df.dropna(inplace= True)
-    
+    df["feature_open"] = df["open"] / df["close"]
+    df["feature_high"] = df["high"] / df["close"]
+    df["feature_low"] = df["low"] / df["close"]
+    df["feature_volume"] = df["volume"] / df["volume"].rolling(7 * 24).max()
+    df.dropna(inplace=True)
+
     return df
 
+
 def reward_function(history):
-    return np.log(history["portfolio_valuation", -1] / history["portfolio_valuation", -2]) #log (p_t / p_t-1 )
+    return np.log(
+        history["portfolio_valuation", -1] / history["portfolio_valuation", -2]
+    )  # log (p_t / p_t-1 )
+
 
 if __name__ == "__main__":
-
     # Uncomment if needed
     # download(
     #     exchange_names = ["binance", "bitfinex2", "huobi"],
@@ -34,23 +35,20 @@ if __name__ == "__main__":
     #     since= datetime.datetime(year= 2019, month= 1, day=1),
     # )
     env = gym.make_vec(
-        id = "MultiDatasetTradingEnv",
-        num_envs = 3,
-
-        dataset_dir = 'examples/data/*.pkl',
-        preprocess = add_features,
-        windows= 5,
-        positions = [ -1, -0.5, 0, 0.5, 1, 1.5, 2],
-        initial_position = 0,
-        trading_fees = 0.01/100,
-        borrow_interest_rate= 0.0003/100,
-        reward_function = reward_function,
-        portfolio_initial_value = 1000,
+        id="MultiDatasetTradingEnv",
+        num_envs=3,
+        dataset_dir="examples/data/*.pkl",
+        preprocess=add_features,
+        windows=5,
+        positions=[-1, -0.5, 0, 0.5, 1, 1.5, 2],
+        initial_position=0,
+        trading_fees=0.01 / 100,
+        borrow_interest_rate=0.0003 / 100,
+        reward_function=reward_function,
+        portfolio_initial_value=1000,
     )
     # Run the simulation
     observation, info = env.reset()
     while True:
         actions = [1, 2, 3]
         observation, reward, done, truncated, info = env.step(actions)
-
-
